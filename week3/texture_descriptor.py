3ca7d6ad65a033c3565e3a52d84ab2fa7ddb32ee
Marcelo <marcelosanchezortega@gmail.com>
2021-10-27 23:38:55 +0200

    first commit all working, need to change HOG params in order to speed up processing time

--- week3/texture_descriptor.py
+++ week3/texture_descriptor.py
@@ -1,15 +1,15 @@
 import cv2
 import scipy
-import bob
 import numpy as np
 import os
 from matplotlib import pyplot as plt
-from similarity import compute_similarity_measure
+from similarity import compute_similarity
 import skimage.feature
+
 
 class TextureDescriptor(object):
 
-    def __init__(self, color_space:str ="gray", scales:int = 3) -> None:
+    def __init__(self, color_space: str = "gray", descriptor: str = "HOG", scales: int = 0) -> None:
         self.color_space_map = {
             "gray": cv2.COLOR_BGR2GRAY,
             "rgb": cv2.COLOR_BGR2RGB,
@@ -25,16 +25,29 @@ class TextureDescriptor(object):
             "lab": self.compute_standard_histogram,
             "lab_3d": self.compute_3d_lab_histogram
         }
+        self.descriptor = descriptor
+        winSize = (64, 64)
+        blockSize = (16, 16)
+        blockStride = (8, 8)
+        cellSize = (8, 8)
+        nbins = 9
+        derivAperture = 1
+        winSigma = 4.
+        histogramNormType = 0
+        L2HysThreshold = 2.0000000000000001e-01
+        gammaCorrection = 0
+        nlevels = 64
         self.descriptor_type = {
-            'HOG': cv2.HOGDescriptor().compute,
+            'HOG': cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
+                                     histogramNormType, L2HysThreshold, gammaCorrection, nlevels).compute,
             'DCT': scipy.fft.dctn,
-            #'GWT':bob.ip.gabor.Transform().transform,
-            'binary_pattern':skimage.feature.local_binary_pattern
-            #skimage.feature.local_binary_pattern(a,8,1)
+            'color_histogram': self.compute_3d_rgb_histogram,
+            # 'GWT':bob.ip.gabor.Transform().transform,
+            'binary_pattern': skimage.feature.local_binary_pattern,
+            # skimage.feature.local_binary_pattern(a,8,1)
         }
         self.color_space = color_space
         self.scales = scales
-
 
     def compute_standard_histogram(self, channels: np.ndarray, mask=None):
         hist_chan = []
@@ -50,11 +63,12 @@ class TextureDescriptor(object):
             3D color histogram (utilizing all channels) with 8 bins in each direction 
         """
 
-        hist = cv2.calcHist([channels], [0, 1, 2], mask, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+        hist = cv2.calcHist([channels], [0, 1, 2], mask, [
+                            8, 8, 8], [0, 256, 0, 256, 0, 256])
         hist = cv2.normalize(hist, hist)
         return hist.flatten()
 
-    def compute_3d_lab_histogram(self, channels:np.ndarray, mask=None):
+    def compute_3d_lab_histogram(self, channels: np.ndarray, mask=None):
         """
           Lab definition:
           L*a*b* breaks the colors down based on human perception and the opponent process of vision. 
@@ -62,8 +76,9 @@ class TextureDescriptor(object):
 
           Then, we will focus on the chromatic representation. Bins has been set empirically
         """
-        
-        hist = ([channels], [0, 1, 2], mask, [2, 24, 24], [0, 256, 0, 256, 0, 256])
+
+        hist = ([channels], [0, 1, 2], mask, [
+                2, 24, 24], [0, 256, 0, 256, 0, 256])
         hist = cv2.normalize(hist, hist)
         return hist.flatten()
 
@@ -72,87 +87,77 @@ class TextureDescriptor(object):
             return np.ones(image.shape[:2]).astype(np.uint8)
         else:
             mask = np.ones(image.shape[:2], dtype="uint8")*255
-            cv2.rectangle(mask, (bbox[0],bbox[1]), (bbox[2],bbox[3]), 0, -1)
+            cv2.rectangle(mask, (bbox[0], bbox[1]), (bbox[2], bbox[3]), 0, -1)
 
             # display the masked region
             #masked = cv2.bitwise_and(image, image, mask=mask)
             #cv2.imshow("Applying the Mask", masked)
             return mask.astype(np.uint8)
 
-    def compute_3d_tiled_histogram(self, channels: np.ndarray, metric:str, bbox:tuple = None):
+    def compute_descriptor(self, channels: np.ndarray, bbox: tuple = None):
         """ input_params
                 bbox: tuple containing two tuples, where first tuple is top-left coordinate of the mask
                 and second tuple in left-bottom coordinates e.g ((0,0),(250,250)), if bbox is None, it will
                 compute the histograms for all the tiles            
         """
-        #if the tile size is not defined at init, we will divide the image in 
+        # if the tile size is not defined at init, we will divide the image in
         histogram = []
-        channels = cv2.cvtColor(channels, self.color_space_map[self.color_space])
-        mask = self.compute_hist_mask(channels, bbox)
-            # substitute the bounding box with the image mean 
+        channels = cv2.cvtColor(
+            channels, self.color_space_map[self.color_space])
+        if bbox is not None:
+            mask = self.compute_hist_mask(channels, bbox)
+            # substitute the bounding box with the image mean
             #channels[bbox[1]:bbox[3],bbox[0]:bbox[2]] = channels.mean(axis=(0,1))
         for pyramid_lvl in range(self.scales+1):
             n_blocks = 2 ** pyramid_lvl
             M = channels.shape[0]//n_blocks
             N = channels.shape[1]//n_blocks
             hist_scale = []
-            for y in range(0,M * n_blocks ,M):
-                for x in range(0, N * n_blocks ,N):
-                    #y1 = y + M
-                    #x1 = x + N
-                    tile = channels[y:y+M,x:x+N]
-                    tile_mask = mask[y:y+M,x:x+N]
-                    #if there is not bbox param, we compute hist for all the tiles
-                    #if bbox is None:
-                    hist = self.histogram_function_map[metric](tile, tile_mask)
+            for y in range(0, M * n_blocks, M):
+                for x in range(0, N * n_blocks, N):
+                    tile = channels[y:y+M, x:x+N]
+                    if bbox is not None:
+                        tile_mask = mask[y:y+M, x:x+N]
+                    print(tile.shape)
+                    # if there is not bbox param, we compute hist for all the tiles
+                    # if bbox is None:
+                    if bbox is not None:
+                        hist = self.descriptor_type[self.descriptor](
+                            tile, mask)
+                    else:
+                        hist = self.descriptor_type[self.descriptor](tile)
                     hist_scale.extend(hist)
 
             histogram.extend(np.stack(hist_scale).flatten())
 
-        return np.stack(histogram).flatten() # Join the histograms and flat them in one dimension array
+        # Join the histograms and flat them in one dimension array
+        return np.stack(histogram).flatten()
 
-    def compute_histogram(self, image: np.ndarray, metric: str, plot=False):
-        """
-        Method to compute the histogram of an image
-        """
-
-        image = cv2.cvtColor(image, self.color_space_map[self.color_space])
-        
-        if not "3d" in metric:
-            image = cv2.split(image)
-
-        hist = self.histogram_function_map[metric](image)
-
-        #hist = hist.astype(np.uint8)
-        if plot:
-            plt.figure()
-            plt.title("Histogram")
-            plt.xlabel("Bins")
-            plt.ylabel("# of Pixels")
-            for color_hist in np.split(hist, len(image)):
-                plt.plot(color_hist)
-            plt.xlim([0, 256])
-            plt.show()
-        return hist
-    
-    def compute_image_similarity(self, query_img, metric, text_extractor_method):
-        self.compute_image_multiscale_similarity(query_img, metric , text_extractor_method)
-
-    def compute_simple_image_similarity(self, query_img, metric:str ):
+    def compute_image_similarity(self, image_dataset, similarity_mode, query_img, text_extractor_method: callable):
         result = []
-        query_img_hist = self.compute_histogram(query_img, metric=metric)
-        for image in self.image_dataset.keys():
-            image_hist = self.compute_histogram(self.image_dataset[image]["image_obj"], metric=metric)
-            sim_result = compute_similarity_measure(image_hist, query_img_hist,self.similarity_mode)
+        if text_extractor_method is not None:
+            bbox_query,  _ = text_extractor_method(query_img, None, None)
+            if bbox_query and self.descriptor != 'color_histogram':
+                print('Only can use bbox with color histogram!!')
+                exit()
+            query_img_hist = self.compute_descriptor(query_img, bbox_query)
+        else:
+            query_img_hist = self.compute_descriptor(query_img)
+        for image in image_dataset.values():
+            image_hist = self.compute_descriptor(image)
+            sim_result = compute_similarity(
+                image_hist, query_img_hist, similarity_mode)
             result.append([image, sim_result])
         return result
 
-    def compute_image_multiscale_similarity(self, query_img, metric:str , text_extractor_method: callable):
-        result = []
-        bbox_query ,  _= text_extractor_method(query_img,None,None)
-        query_img_hist = self.compute_3d_tiled_histogram(query_img, metric, bbox_query)
-        for image in self.image_dataset.keys():
-            image_hist = self.compute_3d_tiled_histogram(self.image_dataset[image]["image_obj"], metric)
-            sim_result = compute_similarity_measure(image_hist, query_img_hist,self.similarity_mode)
-            result.append([image, sim_result])
-        return result
+
+if __name__ == "__main__":
+    img = cv2.imread(
+        '/home/marcelo/Documents/Master_CV/M1/Team3/datasets/BBDD/bbdd_00000.jpg')
+    print(img)
+    query_image = cv2.imread(
+        '/home/marcelo/Documents/Master_CV/M1/Team3/datasets/BBDD/bbdd_00000.jpg')
+    descriptor = TextureDescriptor()
+    image_dataset = {1: query_image}
+    descriptor.compute_image_similarity(
+        image_dataset, 'cosine_similairty', img, None)
